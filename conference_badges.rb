5179800eb31fc0b32db69da0a90935305bdf47f8
Sara Kantor <sara.kantor.nv@gmail.com>
2018-10-08 02:48:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,7 +26,7 @@ end
 def printer(my_list)
   assignments = assign_rooms(my_list)
   my_list.each do |name|
-    puts batch_badge_creator(name)
+    puts batch_badge_creator(my_list)[name])
     puts assignments[name]
   end 
 end 
